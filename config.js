baf6f1f30101e181aa6f52d84f159d074dbd8049
MB <oppoa5s.mbcell@gmail.com>
2025-11-11 19:27:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js — ISI dengan firebaseConfig Anda
 // Contoh: ganti semua nilai dengan milik proyek Anda
 const firebaseConfig = {
-  apiKey: "AIza...yourKey...",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:123456789:web:abcdefg"
+  apiKey: "AIzaSyBr-nd0dxZl5q6DLPxMKLybJ7MyBc9kaBc",
+  authDomain: "realtime-database-f59b8.firebaseapp.com",
+  databaseURL: "https://realtime-database-f59b8-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "realtime-database-f59b8",
+  storageBucket: "realtime-database-f59b8.firebasestorage.app",
+  messagingSenderId: "320982155387",
+  appId: "1:320982155387:web:df67817761e282dee4a2de"
 };
